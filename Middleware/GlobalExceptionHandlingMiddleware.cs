9ebbc509be4db344c51a1e5bfce6f3bd2d5138ce
dhanitha0725 <152945350+dhanitha0725@users.noreply.github.com>
2025-07-26 18:18:16 +0530

    Add project files.

---
+++ Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using ILogger = Serilog.ILogger;
+
+namespace bmsAPI.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware(ILogger logger) : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (ValidationException)
+            {
+                logger.Error("Validation error occurred while processing the request.");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An unhandled exception occurred while processing the request.");
+                throw;
+            }
+        }
+    }
+}
